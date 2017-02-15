eb633368ed2bc18ca669cee51e1fb324b7575d49
Liz Baillie <liz@lizbaillie.com>
2017-02-15 09:03:22 -0800

    WIP

---
+++ app/services/ajax.js
@@ -0,0 +1,20 @@
+import Ember from 'ember';
+import AjaxService from 'ember-ajax/services/ajax';
+import ENV from 'assemble/config/environment';
+
+export default AjaxService.extend({
+  session: Ember.inject.service(),
+  host: ENV.apiHost,
+  headers: Ember.computed('session.authToken', {
+    get() {
+      let headers = {};
+      console.log("HEADERS", this.get('session.data.authenticated.token'));
+      const authToken = this.get('session.data.authenticated.token');
+      const email = this.get('session.data.authenticated.email');
+      if (authToken) {
+        headers['Authorization'] = `Token token="${authToken}", email="${email}"`;
+      }
+      return headers;
+    }
+  })
+});
